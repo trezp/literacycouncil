e49707123f5425568e7059a3c3afaf2744edbd10
Treasure Porth <treasure.rose@gmail.com>
2015-06-15 22:30:53 -0700

    refactored includes, broke accordion

--- js/bcouncil.js
+++ js/bcouncil.js
@@ -1,31 +1,32 @@
+
 $( "#accordion" ).accordion({
-	collapsible: true,
-	active: false,
-	animate: 200,
-	heightStyle: "content"
+  collapsible: true,
+  active: false,
+  animate: 200,
+  heightStyle: "content"
 });
 
+
 $(document).ready(function(){
+
 //when section link is clicked, load that section into the home-content div
 //and remove most-important div CHANGE TO MORE SENSIBLE NAMES 
+  $("#home-content").load("home.html");
+
   $("#classes-link").click(function(){
     $('#home-content').load('classes.html')
-    $('#most-important').remove();
   }); 
 
   $("#volunteer-link").click(function(){
     $('#home-content').load('volunteer.html');
-    $('#most-important').remove();
   }); 
 
   $("#about-link").click(function(){
     $('#home-content').load('about.html');
-    $('#most-important').remove();
   }); 
 
   $("#general-link").click(function(){
     $('#home-content').load('general.html');
-    $('#most-important').remove();
   }); 
 });
 
